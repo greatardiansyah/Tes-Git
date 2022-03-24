95dbb0912ec709b4d4b6e02d85aa90d8b6e0c5fb
greatardiansyah <102204543+greatardiansyah@users.noreply.github.com>
2022-03-24 16:20:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Percobaan menggunakan GIT</title>
+	<h3>
+		TUTORIAL GIT and GITHUB
+	</h3>
+</head>
+<body>
+<p>
+	Hari ini kita pelajari cara penggunaan git dan github
+</p>
+<p>
+	Ayo kita mulai
+</p>
+</body>
+</html>
